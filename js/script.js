1440451870abf0c12fe2bfc3151a37da005142c4
Braga_Ion <bragaion99@gmail.com>
2024-03-14 21:55:10 +0200

    Cards
    Am creat cardurile folosind clasurile, this, functiile constructor

--- js/script.js
+++ js/script.js
@@ -155,4 +155,73 @@ window.addEventListener('DOMContentLoaded', function() {
     }
 
     window.addEventListener('scroll', showModalByScroll);  //adaugam eveniment odata ce am ajut la sfarsit de pag
-});
+
+
+    // Class for Cards
+
+    class MenuCard {  //cream clasul
+        constructor(src, alt, title, descr, price, parentSelector) {  //cream constructorul iar ca argumente dam compenentele cardurilor
+            this.src = src;  //scriem argumentele ca valori
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);  //DOM element 
+            this.transfer = 27;  //curs valuta static ca exemplu
+            this.changeToUAH();  //aici vom chema metoda changeToUAH
+        }
+
+        changeToUAH() {  //metoda pentru a schimba din dolari in grivne
+            this.price = +this.price * this.transfer;  //pentru a afla valoarea in grivne
+        }
+
+        render() {  //cream aspectul siteului (верстка)
+            const element = document.createElement('div');  //cream un div nou iar mai jos ii atribuim o structura noua de elemente 
+            element.innerHTML = `  
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>  
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element);  //punem DOM elementul in noul div 
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        "Меню 'Фитнес'",
+        "Меню 'Фитнес' - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!",
+        9,
+        '.menu .container'  //cream parentul DOM element
+    ).render();  //cream un obiect si deodata chemam metoda render, dupa asta el va disparea pentru ca nu l-am pus intr-o variabila
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        "Меню 'Премиум'",
+        "В меню 'Премиум' мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+        14,
+        '.menu .container'  //cream parentul DOM element
+    ).render();  //cream un obiect si deodata chemam metoda render, dupa asta el va disparea pentru ca nu l-am pus intr-o variabila
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        "Меню 'Постное'",
+        "Меню 'Постное' - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+        21,
+        '.menu .container'  //cream parentul DOM element
+    ).render();  //cream un obiect si deodata chemam metoda render, dupa asta el va disparea pentru ca nu l-am pus intr-o variabila
+
+
+}); 
+
+
+
